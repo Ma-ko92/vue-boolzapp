bdb66f370b522165f4d97d7010f2753c24c7b6e7
Mario.Koco <mario.koco92@gmail.com>
2021-04-26 16:04:32 +0200

    sistemato meglio commenti ed eliminate varibili settate per prova

--- js/script.js
+++ js/script.js
@@ -5,25 +5,25 @@ var app = new Vue({
 
     data:{
        
-        // Index
+        // Current active user.
         activeContact: 0,
 
-        // User message input
+        // User message input.
         newMessageText: "",
 
-        // User search input
+        // User search input.
         searchTextFilter: "",
 
-        // set the message options active
+        // status of the current active message.
         activeMessage: false,
 
-        // User personal Profile
+        // User personal Profile.
         userProfile: {
             name: "Mario",
             avatar: "_7"
         },
 
-        // User Contacts
+        // User Contacts.
         contacts: [
             {
                 name: 'Michele',
@@ -33,20 +33,17 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received',
-                        dropDownMenu: false
+                        status: 'received'
                     }
                 ],
             },
@@ -58,20 +55,17 @@ var app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received',
-                        dropDownMenu: false
+                        status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     }
                 ],
             },
@@ -83,20 +77,17 @@ var app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received',
-                        dropDownMenu: false
+                        status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received',
-                        dropDownMenu: false
+                        status: 'received'
                     }
                 ],
             },
@@ -108,14 +99,12 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent',
-                        dropDownMenu: false
+                        status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received',
-                        dropDownMenu: false
+                        status: 'received'
                     }
                 ],
             },
